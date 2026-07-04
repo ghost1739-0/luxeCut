322f6f277a90e7379d14fa700f1bb706cb372b43
ghost1739-0 <valorantyeni3939@gmail.com>
2026-07-04 23:21:11 +0300

    Fix PWA config for production3

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -74,7 +74,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     ],
     links: [
       { rel: "stylesheet", href: appCss },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      { rel: "icon", href: "/pwa-512x512.png", type: "image/x-icon" },
     ],
     scripts: [{
       type: "application/ld+json",
